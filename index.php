54874fe357677e3183491f862f893e49e86631d8
joy631pu <97850075+joy631pu@users.noreply.github.com>
2023-10-15 01:55:52 -0700

    Create stock page

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" href="index.php">Stock</a>
+          <a class="nav-link" href="stock.php">Stock</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="purchase.php">Purchase</a>
---
+++ stock.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<title>Comox Fashion</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+    <h5>Stock Status</h5>
+    <table class="table table-striped">
+  <thead>
+    <tr>
+      <!--<th scope="col">#</th>-->
+      <th scope="col">Product Name</th>
+      <th scope="col">Description</th>
+      <th scope="col">Unit</th>
+      <th scope="col">Unit Price</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+  </tbody>
+</table>
+</div>
+</body>
+</html>
