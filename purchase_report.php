5d4d0deeb227da9c341e861bcb57d22f6afd9e5c
joy631pu <97850075+joy631pu@users.noreply.github.com>
2023-10-15 16:57:17 -0700

    Create purchase_report page

---
+++ purchase_report.php
@@ -0,0 +1,38 @@
+<?php
+include "index.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+<form>
+  <label for="starttime">Start (date and time):</label>
+  <input type="datetime-local" id="starttime" name="starttime">
+
+  <label for="endtime"> End (date and time):</label>
+  <input type="datetime-local" id="endtime" name="endtime">
+  <input type="submit" name="submit">
+</form>
+<button type="button" onclick="window.print();return false;">Pdf Report</button>
+<h5>Purchase Report</h5>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <!--<th scope="col">#</th>-->
+      <th scope="col">Product Name</th>
+      <th scope="col">Unit</th>
+      <th scope="col">Total Unit Price</th>
+    </tr>
+  </thead>
+  <tbody>
+  </tbody>
+</table>
+</div>
+</body>
+</html>
