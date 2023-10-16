6532fddf6b0e03a612181fbd29140fe5c3847d00
joy631pu <97850075+joy631pu@users.noreply.github.com>
2023-10-15 17:10:45 -0700

    Create sales page

---
+++ sales.php
@@ -0,0 +1,28 @@
+<?php
+include "index.php";
+?>
+<html>
+<head>
+    <title>Comox Fashion</title>
+</head>
+<body>
+    <div class="container">
+    <h5>Sales</h5>
+    <table class="table table-striped">
+  <thead>
+    <tr>
+      <!--<th scope="col">#</th>-->
+      <th scope="col">Product Name</th>
+      <th scope="col">Description</th>
+      <th scope="col">Unit</th>
+      <th scope="col">Unit Price</th>
+      <th scope="col">Sell Unit</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+  </tbody>
+</table>
+</div>
+</body>
+</html>
