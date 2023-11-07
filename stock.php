4b06af0481eec61bac1e25e15509ff0e262f5531
joy631pu <97850075+joy631pu@users.noreply.github.com>
2023-11-06 22:17:32 -0800

    Add user authentication to stock page

--- stock.php
+++ stock.php
@@ -1,5 +1,20 @@
 <!-- include index.php -->
+
 <?php
+SESSION_START();
+
+if(isset($_SESSION['auth']))
+{
+   if($_SESSION['auth']!=1)
+   {
+       header("location:index.php");
+   }
+}
+else
+{
+   header("location:index.php");
+}
+
 include "nav.php";
 include "views/stock.view.php";
 ?>
