16ba3fab25ebba16a81267986f54982c7b8fe4bc
joy631pu <97850075+joy631pu@users.noreply.github.com>
2023-11-06 23:16:19 -0800

    Fix error on main page

--- main.php
+++ main.php
@@ -61,7 +61,7 @@ if(isset($_GET['remove'])){
                 <td><input type="number" name="update_unit"  value="<?php echo $row['unit'];?>"></td>
                 <td><input type="number" name="update_unitprice"  value="<?php echo $row['unitprice'];?>"></td>
                 <td><button type="submit" class="btn btn-primary" name="update_btn">update</button></td>
-                <td><a  class="btn btn-primary" href="index.php?remove=<?php echo $row['id']; ?>">delete</a></td>
+                <td><a  class="btn btn-primary" href="main.php?remove=<?php echo $row['id']; ?>">delete</a></td>
                 </tr>
                 </form>
                 <?php }
--- purchase.php
+++ purchase.php
@@ -1,5 +1,33 @@
-<!-- include index.php file -->
+<!-- Purchase or add new product to the product table -->
 <?php
 include "nav.php";
+include "connection.php";
 include "views/purchase.view.php";
+if (isset($_POST['submit'])) 
+    {
+    $name=$_POST['name'];
+    $des=$_POST['des'];
+    $unit=$_POST['unit'];
+    $unitprice=$_POST['unitprice'];
+
+    $insertsql = "INSERT INTO product(name, des, unit, unitprice) VALUES ('$name', '$des', '$unit','$unitprice')";
+
+    $insertsql1 = "INSERT INTO purchase(name, des, unit, unitprice) VALUES ('$name', '$des', '$unit','$unitprice')";
+    if ($conn->query($insertsql1) === TRUE) 
+    {
+      echo "";
+    } else 
+    {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    
+    if ($conn->query($insertsql) === TRUE) 
+    {
+      echo "   New record created successfully";
+    } else 
+    {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    } 
 ?>
