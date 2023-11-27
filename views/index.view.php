6818d3ea253687776ed1462fd60876e19ba0c51c
joy631pu <97850075+joy631pu@users.noreply.github.com>
2023-11-27 00:01:15 -0800

    Set PhP error levels

--- views/index.view.php
+++ views/index.view.php
@@ -1,5 +1,9 @@
 <!-- login page -->
 <?php 
+error_reporting(E_ALL & ~E_NOTICE); // ALL EXCEPT NOTICES
+ini_set("log_errors", 1); // SAVE ERROR TO LOG FILE
+ini_set("error_log", __DIR__ . DIRECTORY_SEPARATOR . "error.log"); // LOG FILE
+ini_set("display_errors", 1);
 // User Session
 SESSION_START();
 // Password verification
